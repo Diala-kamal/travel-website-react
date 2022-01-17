7e643f665b602687d7c164f9d481cae03b9fbdbb
2314-max <dialakamal747@gmail.com>
2022-01-17 10:08:48 +0200

    add files

---
+++ src/components/Button.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Button.css";
+import { Link } from "react-router-dom";
+const STYLES = ["btn--primary", "btn-outline"];
+const SIZES = ["btn--meduim ", "btn--large"];
+const Button = ({ children, type, onClick, buttonStyle, buttonSize }) => {
+  const checkButtonstyle = STYLES.includes(buttonStyle)
+    ? buttonStyle
+    : STYLES[0];
+
+  const checkButtonSizes = STYLES.includes(buttonSize) ? buttonSize : SIZES[0];
+  return (
+    <Link to="sign-up" className="btn-mobile">
+      <button
+        className={`btn ${checkButtonstyle} ${checkButtonSizes}`}
+        onClick={onClick}
+        type={type}
+      >
+        {children}
+      </button>
+    </Link>
+  );
+};
+
+export default Button;
